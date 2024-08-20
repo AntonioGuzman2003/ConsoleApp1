e7c019a95bb6a7eb2b3303fa457e21f3c5adb440
Alumno <Alumno@LAB1507-03>
2024-08-20 17:26:43 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Persona
+    { 
+        public Persona() { }
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string NombresCompletos { get; set; }
+
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombre + " " + Apellidos;
+        }
+    
+    }
+}
---
+++ ConsoleApp1/app1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class app1
+    {
+        static void Main(string[] args)
+        {
+            int area = 0;
+            int perimetro = 0;
+
+
+            Persona persona = new Persona
+            {
+                Nombres = "Antonio",
+                Apellidos = "Guzman"
+
+            };
+
+            persona.AsignarNombresCompletos();
+            Console.WriteLine("El nombre es: ");
+            Console.WriteLine(persona.AsignarNombresCompletos);
+
+            Rectangulo rectangulo = new Rectangulo();
+            rectangulo.Base = 10;
+            rectangulo.Altura = 20;
+
+            area = rectangulo.CalcularArea();
+
+            perimetro = rectangulo.CalcularPerimetro();
+
+            Console.WriteLine("El area es: ");
+            Console.WriteLine(area);
+
+            Console.WriteLine("El perimetro es: ");
+            Console.WriteLine(perimetro);
+
+            Console.WriteLine("Hola mundo");
+            Console.Read();
+        }
+    }
+}
